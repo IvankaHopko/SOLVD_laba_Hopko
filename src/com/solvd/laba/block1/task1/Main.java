56eaa8ab95466ee0db4b6a0d5c4822d48214e2b5
IvankaHopko <145485014+IvankaHopko@users.noreply.github.com>
2023-11-01 20:16:28 +0200

    task one and task two

---
+++ src/com/solvd/laba/block1/task1/Main.java
@@ -0,0 +1,10 @@
+package com.solvd.laba.block1.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Ivanna";
+        String lastName = "Hopko";
+
+        System.out.println("Hello! Nice to see you, " + firstName + " " + lastName);
+    }
+}
