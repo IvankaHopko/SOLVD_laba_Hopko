21644616443bd615222b6334a60a8245adeca7ee
IvankaHopko <145485014+IvankaHopko@users.noreply.github.com>
2023-11-02 17:49:31 +0200

    Changes to task1

--- src/com/solvd/laba/block1/task1/Main.java
+++ src/com/solvd/laba/block1/task1/Main.java
@@ -1,10 +1,30 @@
 package com.solvd.laba.block1.task1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String firstName = "Ivanna";
-        String lastName = "Hopko";
+        //Scanner scan = new Scanner(System.in);
+        //String firstName = scan.nextLine();
+        //String lastName = scan.nextLine();
+        //System.out.println("Hello" + " " + firstName + lastName);
 
-        System.out.println("Hello! Nice to see you, " + firstName + " " + lastName);
-    }
-}
+        String firstName;
+        String lastName;
+
+        if(args.length == 0)
+        {
+            System.out.println("Empty arguments");
+        }
+
+        if(args.length == 2)
+        {
+            firstName  = args[0];
+            lastName  = args[1];
+            System.out.println("Hello nice to see You: " + args[0] + " " + args[1] +".");
+        }
+
+        }
+        }
+
+
