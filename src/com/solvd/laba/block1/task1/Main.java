edbaa4db2030c3ffef84e3b75e60209e8b67d5de
IvankaHopko <145485014+IvankaHopko@users.noreply.github.com>
2023-11-02 19:24:54 +0200

    Corrected task1

--- src/com/solvd/laba/block1/task1/Main.java
+++ src/com/solvd/laba/block1/task1/Main.java
@@ -4,27 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Scanner scan = new Scanner(System.in);
-        //String firstName = scan.nextLine();
-        //String lastName = scan.nextLine();
-        //System.out.println("Hello" + " " + firstName + lastName);
-
-        String firstName;
-        String lastName;
-
-        if(args.length == 0)
-        {
-            System.out.println("Empty arguments");
-        }
-
-        if(args.length == 2)
-        {
-            firstName  = args[0];
-            lastName  = args[1];
-            System.out.println("Hello nice to see You: " + args[0] + " " + args[1] +".");
-        }
-
-        }
-        }
-
+        Scanner scan = new Scanner(System.in);
+        String firstName = scan.nextLine();
+        String lastName = scan.nextLine();
+        System.out.println("Hello! Nice to see you," + " " + firstName + " " + lastName + ".");
+    }
+}
 
