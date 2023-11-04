8959539674bd7bc7733669317e771c934ab9069e
IvankaHopko <145485014+IvankaHopko@users.noreply.github.com>
2023-11-04 16:01:31 +0200

    Final corrections to task1

--- src/com/solvd/laba/block1/task1/Main.java
+++ src/com/solvd/laba/block1/task1/Main.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String firstName = scan.nextLine();
-        String lastName = scan.nextLine();
-        System.out.println("Hello! Nice to see you," + " " + firstName + " " + lastName + ".");
+        String firstName;
+        String lastName;
+
+        if(args.length == 2) {
+            firstName = args[0];
+            lastName = args[1];
+            System.out.println("Hello nice to see You: " + args[0] + " " + args[1] + ".");
+        } else
+            System.out.println("Enter your data");
     }
 }
+
 
